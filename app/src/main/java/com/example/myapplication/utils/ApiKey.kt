ea48284291860c72b7049dae278c7eb13f2633b0
dvirfriedler <dvir.friedler@gmail.com>
2023-08-25 17:43:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/utils/ApiKey.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.utils
+
+
+object ApiKey {
+    private const val apiKey: String = "2b06fa5aaemshe43b48e689f6b9ap19f92ajsndf054853040a"
+
+    fun getApiKey(): String {
+        return apiKey
+    }
+}
