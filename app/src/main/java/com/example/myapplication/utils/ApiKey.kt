b4dfeed02972ea88696548c45ddf3f9122701e63
dvirfriedler <dvir.friedler@gmail.com>
2024-03-20 14:37:08 +0200

    edit some code

--- app/src/main/java/com/example/myapplication/utils/ApiKey.kt
+++ app/src/main/java/com/example/myapplication/utils/ApiKey.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.utils
 
 
 object ApiKey {
-    private const val apiKey: String = "2b06fa5aaemshe43b48e689f6b9ap19f92ajsndf054853040a"
+    private const val apiKey: String = ""
 
     fun getApiKey(): String {
         return apiKey
